73181d928f6b7fc614d0fe48cff1ca4cad61be71
DirsanA <dirsanantehun739@gmail.com>
2024-12-26 23:32:19 -0800

    modularization of code is done

--- tic_toc_toe/src/Game.jsx
+++ tic_toc_toe/src/Game.jsx
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { combine } from "zustand/middleware";
-import Square from "./Square";
+import Board from "./Board";
 const useGameStore = create(
   combine(
     {
@@ -29,44 +29,6 @@ const useGameStore = create(
     }
   )
 );
-
-function Board({ xIsNext, squares, onPlay }) {
-  const winner = calculateWinner(squares);
-  const turns = calculateTurns(squares);
-  const player = xIsNext ? "X" : "O";
-  const status = calculateStatus(winner, turns, player);
-
-  function handleClick(i) {
-    if (squares[i] || winner) return;
-    const nextSquares = squares.slice();
-    nextSquares[i] = player;
-    onPlay(nextSquares);
-  }
-
-  return (
-    <>
-      <div style={{ marginBottom: "0.5rem" }}>{status}</div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gridTemplateRows: "repeat(3, 1fr)",
-          width: "calc(3 * 2.5rem)",
-          height: "calc(3 * 2.5rem)",
-          border: "1px solid #999",
-        }}
-      >
-        {squares.map((_, i) => (
-          <Square
-            key={`square-${i}`}
-            value={squares[i]}
-            onSquareClick={() => handleClick(i)}
-          />
-        ))}
-      </div>
-    </>
-  );
-}
 
 export default function Game() {
   const { history, setHistory, currentMove, setCurrentMove } = useGameStore();
@@ -114,36 +76,4 @@ export default function Game() {
       </div>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return null;
-}
-
-function calculateTurns(squares) {
-  return squares.filter((square) => !square).length;
-}
-
-function calculateStatus(winner, turns, player) {
-  if (!winner && !turns) return "Draw";
-  if (winner) return `Winner ${winner}`;
-  return `Next player: ${player}`;
 }
